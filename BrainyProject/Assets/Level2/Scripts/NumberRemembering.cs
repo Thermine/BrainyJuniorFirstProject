cf3dacbc4bd6311d91927fd52b61d822d9b5f4b9
Sokotukhin Ivan <ivansokotuhin1816@gmail.com>
2023-04-16 15:29:42 +0700

    лифт работай пж

---
+++ BrainyProject/Assets/Level2/Scripts/NumberRemembering.cs
@@ -0,0 +1,38 @@
+using Pixelcrew;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberRemembering : MonoBehaviour
+{
+    public string Player_Combination;
+    public ObjectSwitcher ObjectSwitcher;
+    public int amount;
+    public GameObject[] btns;
+    SaveDataInFileComponent SaveDataInFileComponent;
+    private void Start()
+    {
+        SaveDataInFileComponent = GameObject.FindWithTag("DataSaver").GetComponent<SaveDataInFileComponent>();
+    }
+    public void vvod(string type)
+    {
+        amount += 1;
+        if (amount <= ObjectSwitcher.FigureIndexess.Count)
+        {
+            Player_Combination += " " + type;
+        }
+        if (amount == ObjectSwitcher.FigureIndexess.Count)
+        {
+            StartCoroutine(wait());
+        }
+    }
+    IEnumerator wait()
+    {
+        SaveDataInFileComponent.WriteCsv(Player_Combination);
+        yield return new WaitForSeconds(0.5f);
+        for (int i = 0; i < btns.Length; i++)
+        {
+            btns[i].SetActive(false);
+        }
+    }
+}
