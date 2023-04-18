f86a3fe43dae3801a8c31148a117dbd555e3d46d
Djastas <timebrocop@gmail.com>
2023-04-18 17:08:16 +0500

    rewrite the database on singlton

---
+++ Assets/_Main/Scripts/Managers/SaveDataInFileManager.cs
@@ -0,0 +1,79 @@
+using System;
+using System.IO;
+using StvDEV.StarterPack;
+using UnityEngine;
+
+namespace BrainyJunior.MyGame.Scripts.Managers
+{
+    public class SaveDataInFileManager : MonoBehaviourSingleton<AudioManager>
+    {
+        private TextWriter _tw; 
+        private string _fullPathAndFileName;
+        
+
+        [SerializeField] private string fileNameAndPath = "/test.csv";
+        
+        
+
+        
+        [ContextMenu("write")]
+        public void WriteCsv(string i)
+        {
+            _tw = new StreamWriter(_fullPathAndFileName, true);
+            _tw.Write(i + ",");
+            _tw.Close();
+
+        }
+        
+        public void WriteEnterCsv(string i)
+        {
+            _tw = new StreamWriter(_fullPathAndFileName, true);
+            _tw.Write("\n");
+            _tw.Close();
+
+        }
+
+        private void Start()
+        {
+            _fullPathAndFileName = Application.dataPath + fileNameAndPath; 
+           
+        }
+
+
+        
+        
+     
+
+        //[SerializeField] private TextAsset textAssetForRead;
+        // public PlayerList playerList = new PlayerList();
+        // [ContextMenu("read")]
+        // public void ReadCvs()
+        // {
+        //     string[] data = textAssetForRead.text.Split(new string[] {",", "\n"}, StringSplitOptions.None);
+        //     int tableSize = data.Length / 2 - 1;
+        //     playerList.players = new Player[tableSize];
+        //     for (int i = 0; i < tableSize; i++)
+        //     {
+        //         playerList.players[i] = new Player();
+        //         playerList.players[i].name = data[2* (i + 1)];
+        //         playerList.players[i].rer = data[2* (i + 1 ) + 1];
+        //     }
+        // } не используемый метод
+
+        
+        
+        // не используемый класс
+        public class Player
+        {
+            public string name;
+            public string rer;
+        }
+        
+        [Serializable]
+        public class PlayerList
+        {
+            public Player[] players;
+        }
+
+    }
+}
