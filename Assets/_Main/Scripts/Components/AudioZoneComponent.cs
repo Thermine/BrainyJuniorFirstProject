33c71d288b8b1ffdd64c113f1a050349e8ee911f
Djastas <timebrocop@gmail.com>
2023-05-13 20:49:14 +0500

    Add background music to interact system
    use PlayAudioById(string id, bool loop = false , bool isBackground = false) to play sound and use PlayAudioById(string id, bool loop = true , bool isBackground = true) to play background music
    
    PlayAudioById return number in list (int) use this value to DeleteAudioNum(int num),PlayAudioNum(int num) or StopAudioNum(int num)

---
+++ Assets/_Main/Scripts/Components/AudioZoneComponent.cs
@@ -0,0 +1,14 @@
+using System;
+using BrainyJunior.MyGame.Scripts.Managers;
+using UnityEngine;
+
+public class AudioZoneComponent : MonoBehaviour
+{
+    [SerializeField] private string triggerTag;
+    [SerializeField] private string audioId;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!other.CompareTag(triggerTag)) return;
+        AudioManager.Instance.PlayAudioById(audioId);
+    }
+}
